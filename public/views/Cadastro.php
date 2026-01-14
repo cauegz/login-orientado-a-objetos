fb881f860b96927c19fcc8e4cf4773382c1f7653
cauegz <cauegeraldizanardi@gmail.com>
2026-01-14 13:26:38 -0300

    Alterações no estilo e JavaScript básico

--- public/views/Cadastro.php
+++ public/views/Cadastro.php
@@ -6,47 +6,100 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://kit.fontawesome.com/c25eca0384.js" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
+<?php
+session_start();
+if (isset($_SESSION['log'])) {
+    echo '<div class="caixa-erro"><i class="fa-solid fa-triangle-exclamation" style="color:yellow;"></i><p class="paragrafo-erro">' . $_SESSION['log'] . '</p></div>';
+    unset($_SESSION['log']);
+}
+?>
 
 <body class="d-flex w-100 justify-content-center align-items-center" style="height:100vh;">
     <div class="rounded shadow-lg w-25 h-auto d-flex flex-column align-items-center justify-content-center gap-3">
-        <?php
-            session_start();
-            if(isset($_SESSION['log'])){
-                echo $_SESSION['log'];
-                unset ($_SESSION['log']);
-            }
-        ?>
-        <form action="index.php?rota=Cadastro&acao=cadastrar" method="POST" class="d-flex flex-column gap-2">
+        <h1>Faça seu cadastro</h1>
+        <form action="index.php?rota=Cadastro&acao=cadastrar" onsubmit="return verifica_senha('password', 'password-confirm')" method="POST" class="d-flex flex-column gap-2 flex-wrap">
             <div class="input-group">
-                <input type="text" name="name" placeholder="Usuario">
+                <span class="input-group-text">
+                    <i class="fa-solid fa-user"></i>
+                </span>
+                <input type="text" name="name" placeholder="Usuario" class="form-control">
             </div>
 
             <div class="input-group">
-                <input type="email" name="email" placeholder="Email">
+                <span class="input-group-text">
+                    <i class="fa-solid fa-envelope"></i>
+                </span>
+                <input type="email" name="email" placeholder="Email" class="form-control">
             </div>
 
             <div class="input-group">
-                <input type="text" name="phone" placeholder="Telefone">
+                <span class="input-group-text">
+                    <i class="fa-solid fa-phone"></i>
+                </span>
+                <input type="text" name="phone" placeholder="Telefone" class="form-control">
             </div>
 
             <div class="input-group">
-                <input type="password" name="password" placeholder="Senha">
+                <span class="input-group-text">
+                    <i class="fa-solid fa-lock"></i>
+                </span>
+                <input type="password" name="password" id="password" placeholder="Senha" class="form-control">
+                <span class="input-group-text" id="olho-span" onclick="muda_senha('olho-i', 'password')">
+                    <i class="fa-solid fa-eye" id="olho-i"></i>
+                </span>
+            </div>
+            <div class="input-group">
+                <span class="input-group-text">
+                    <i class="fa-solid fa-lock"></i>
+                </span>
+                <input type="password" id="password-confirm" placeholder="Confirme a senha" class="form-control">
+                <span class="input-group-text" id="olho-span-confirm"
+                    onclick="muda_senha('olho-i-confirm', 'password-confirm')">
+                    <i class="fa-solid fa-eye" id="olho-i-confirm"></i>
+                </span>
             </div>
 
             <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-primary">
+                <span id="erro-senha"></span>
+                <button type="submit"  class="btn btn-primary">
                     Enviar
                 </button>
             </div>
             <p>Já possui uma conta? <a href="index.php?rota=Auth&acao=login">Login</a></p>
         </form>
     </div>
+    <script>
+        function muda_senha(olho_i, password) {
+            var olho = document.getElementById(olho_i);
+            var input_senha = document.getElementById(password);
+            if (input_senha.type == "text") {
+                input_senha.type = "password";
+                olho.classList = "fa-solid fa-eye";
+            } else {
+                input_senha.type = "text";
+                olho.classList = "fa-solid fa-eye-slash";
+            }
+        }
+        function verifica_senha(senha1, senha2) {
+            var senha1 = document.getElementById(senha1).value;
+            var senha2 = document.getElementById(senha2).value;
+            var erro = document.getElementById('erro-senha');
 
+            if (senha1 != senha2) {
+                erro.innerHTML = `
+            As senhas não coincidem
+        `;
+                return false;
+            }
+            return true;
+        }
+    </script>
 </body>
 
 </html>
--- public/views/Login.php
+++ public/views/Login.php
@@ -6,31 +6,36 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://kit.fontawesome.com/c25eca0384.js" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
+<?php
+session_start();
+if (isset($_SESSION['log'])) {
+    echo '<div class="caixa-erro"><i class="fa-solid fa-triangle-exclamation" style="color:yellow;"></i><p class="paragrafo-erro">' . $_SESSION['log'] . '</p></div>';
+    unset($_SESSION['log']);
+}
+?>
 
 <body class="d-flex w-100 justify-content-center align-items-center" style="height:100vh;">
-    <div class="rounded shadow-lg w-25 h-25 flex-column d-flex align-items-center justify-content-center gap-3">
-        <?php
-            session_start();
-            if(isset($_SESSION['log'])){
-                echo $_SESSION['log'];
-                unset ($_SESSION['log']);
-            }
-        ?>
-        <form action="index.php" method="POST" class=" d-flex flex-column gap-2">
-            <div class="input-group">
-                <input type="email" name="email" placeholder="Email">
+    <div class="rounded shadow-lg w-auto h-auto p-3 flex-column d-flex align-items-center justify-content-center gap-3">
+        <h1>Sistema de agenda de contatos</h1>
+
+        <form action="index.php" method="POST">
+            <div class="input-group mb-3">
+                <span class="input-group-text"><i class="fa-solid fa-envelope"></i></span>
+                <input type="email" name="email" class="form-control" placeholder="Email">
             </div>
 
-            <div class="input-group">
-                <input type="password" name="password" placeholder="Senha">
+            <div class="input-group mb-3">
+                <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
+                <input type="password" name="password" placeholder="Senha" class="form-control">
             </div>
 
-            <div class="d-grid gap-2">
+            <div class="d-grid">
                 <button type="submit" class="btn btn-primary">
                     Enviar
                 </button>
